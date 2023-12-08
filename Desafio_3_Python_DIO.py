fb2226f57c44e433053d15d0a5663f767e6afe4c
Vinni-Teodoro <vinicius_adriano_teodoro@hotmail.com>
2023-12-07 21:16:18 -0300

    Initial commit

---
+++ Desafio_3_Python_DIO.py
@@ -0,0 +1,20 @@
+N = int(input())
+
+
+
+if N > 0:
+
+    for _ in range(N):
+
+        A, B = input().split()
+
+
+
+        if A.endswith(B):
+
+            print("encaixa")
+
+        else:
+
+            print("nao encaixa")  
+
